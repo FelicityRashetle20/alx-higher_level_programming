bda428020c167b1bb35748541349d49b148b826c
FelicityRashetle20 <felichrist50@gmail.com>
2023-08-28 14:05:50 -0700

    Update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-#Assume that a and b are integers
 def safe_print_division(a, b):
     try:
         final = a / b
-    except:
+    except IndexError:
         final = None
     finally:
         print("Inside result: {}".format(final))
