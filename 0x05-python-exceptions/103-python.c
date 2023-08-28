fb039adbc7ac70435ec6867df07e6a449decbe7d
FelicityRashetle20 <felichrist50@gmail.com>
2023-08-28 15:31:52 -0700

    update

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,15 +2,14 @@
 #include <Python.h>
 
 /**
- * print_python_bytes - Prints bytes information
- *
- * @p: Python Object
- * Return: no return
+ * print_python_bytes - Prints a maximum of 10 bytes
+ * @p: Python Byte Object
+ * If p is not valid, print error message
  */
 void print_python_bytes(PyObject *p)
 {
-	char *string;
-	long int size, i, limit;
+	char *byte_word;
+	long int weight, i, bound;
 
 	setbuf(stdout, NULL);
 
@@ -22,65 +21,39 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	size = ((PyVarObject *)(p))->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
+	weight = ((PyVarObject *)(p))->ob_weight;
+	byte_word = ((PyBytesObject *)p)->ob_sval;
 
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
+	printf("  size: %ld\n", weight);
+	printf("  trying string: %s\n", byte_word);
 
-	if (size >= 10)
-		limit = 10;
+	if (weight >= 10)
+		bound = 10;
 	else
-		limit = size + 1;
+		bound = weight + 1;
 
-	printf("  first %ld bytes:", limit);
+	printf("  first %ld bytes:", bound);
 
-	for (i = 0; i < limit; i++)
-		if (string[i] >= 0)
-			printf(" %02x", string[i]);
+	for (i = 0; i < bound; i++)
+		if (byte_word[i] >= 0)
+			printf(" %02x", byte_word[i]);
 		else
-			printf(" %02x", 256 + string[i]);
+			printf(" %02x", 256 + byte_word[i]);
 
 	printf("\n");
 	setbuf(stdout, NULL);
 }
 
 /**
- * print_python_float - Prints float information cbhyjmns
- */
-void print_python_float(PyObject *p)
-{
-	double val;
-	char *nf;
-
-	setbuf(stdout, NULL);
-	printf("[.] float object info\n");
-
-	if (!PyFloat_Check(p))
-	{
-		printf("  [ERROR] Invalid Float Object\n");
-		setbuf(stdout, NULL);
-		return;
-	}
-
-	val = ((PyFloatObject *)(p))->ob_fval;
-	nf = PyOS_double_to_string(val, 'r', 0, Py_DTSF_ADD_DOT_0, Py_DTST_FINITE);
-
-	printf("  value: %s\n", nf);
-	setbuf(stdout, NULL);
-}
-
-/**
- * print_python_list - Prints list information
- *
- * @p: Python Object
- * Return: no return
+ * print_python_list - Prints information of list
+ * @p: Python Byte Object
+ * ip @p not valid, print error message
  */
 void print_python_list(PyObject *p)
 {
-	long int size, i;
-	PyListObject *list;
-	PyObject *obj;
+	long int weight, i;
+	PyListObject *point_list;
+	PyObject *obj_pointer;
 
 	setbuf(stdout, NULL);
 	printf("[*] Python list info\n");
@@ -92,21 +65,48 @@ void print_python_list(PyObject *p)
 		return;
 	}
 
-	size = ((PyVarObject *)(p))->ob_size;
-	list = (PyListObject *)p;
+	weight = ((PyVarObject *)(p))->ob_weight;
+	point_list = (PyListObject *)p;
 
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
+	printf("[*] Size of the Python List = %ld\n", weight);
+	printf("[*] Allocated = %ld\n", point_list->allocated);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < weight; i++)
 	{
-		obj = list->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		obj_pointer = point_list->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj_pointer)->ob_type)->tp_name);
 
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
-		if (PyFloat_Check(obj))
-			print_python_float(obj);
+		if (PyBytes_Check(obj_pointer))
+			print_python_bytes(obj_pointer);
+		if (PyFloat_Check(obj_pointer))
+			print_python_float(obj_pointer);
 	}
+	setbuf(stdout, NULL);
+}
+
+/**
+ * print_python_float - Prints information of float
+ * @p: Python Byte Object
+ * If @p not valid, print error message
+ */
+void print_python_float(PyObject *p)
+{
+	double value;
+	char *new_pointer;
+
+	setbuf(stdout, NULL);
+	printf("[.] float object info\n");
+
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		setbuf(stdout, NULL);
+		return;
+	}
+
+	value = ((PyFloatObject *)(p))->ob_fval;
+	new_pointer = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, Py_DTST_FINITE);
+
+	printf("  value: %s\n", new_pointer);
 	setbuf(stdout, NULL);
 }
