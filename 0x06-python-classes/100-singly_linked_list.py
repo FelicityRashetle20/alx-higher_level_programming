f20359be19648c1d7c68221b148aa01321d52e40
FelicityRashetle20 <felichrist50@gmail.com>
2023-08-29 08:26:07 -0700

    advanced tasks

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""The documentation as required by ALX program
+"""
+
+
+class Node:
+    """The documentation as required by ALX program
+    """
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+"""The documentation as required by ALX program
+"""
+
+
+class SinglyLinkedList:
+   """The documentation as required by ALX program
+   """
+    def __str__(self):
+        return_data = ""
+        node_pointer = self.__head
+
+        while node_pointer is not None:
+            return_data += str(node_pointer.data)
+            if node_pointer.next_node is not None:
+                return_data += "\n"
+            node_pointer = node_pointer.next_node
+
+        return return_data
+
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        node_pointer = self.__head
+
+        while node_pointer is not None:
+            if node_pointer.data > value:
+                break
+            node_pointer_prev = node_pointer
+            node_pointer = node_pointer.next_node
+
+        newNode = Node(value, node_pointer)
+        if node_pointer == self.__head:
+            self.__head = newNode
+        else:
+            node_pointer_prev.next_node = newNode
---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Documentation as required by the ALX program
+"""
+
+
+class Square:
+    """ Documentation as required by the ALX program
+    """
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
+
+    def __init__(self, size=0):
+        """ Documentation as required by the ALX program
+    	"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Documentation as required by the ALX program
+    	"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ Documentation as required by the ALX program
+    	"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Documentation as required by the ALX program
+    	"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
