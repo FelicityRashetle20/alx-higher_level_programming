bea83e3b395a248a361b812a7a39dbe321cc0658
FelicityRashetle20 <felichrist50@gmail.com>
2023-08-29 07:53:02 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""The documentation as required by ALX program
+"""
+
+
 class Square:
+    """The documentation as required by ALX program
+    """
     def __init__(self, size):
         self.__size = size
