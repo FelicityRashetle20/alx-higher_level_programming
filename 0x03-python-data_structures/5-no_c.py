669633859deb3130a7eb20883ed69196e2b50728
FelicityRashetle20 <felichrist50@gmail.com>
2023-08-30 07:03:37 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,6 +6,6 @@ def no_c(my_string):
     for val in range(count):
         if (my_string[i] == 'c' or my_string[i] == 'C'):
             curr_string = curr_string[:(val - num)] + my_string[(val + 1):]
-            j += 1
+            val += 1
     return(curr_string)
 
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def limiteger(my_list=[]):
+def max_integer(my_list=[]):
     count = len(my_list)
     if count == 0:
         return (None)
