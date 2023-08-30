655150021fd6022f0af2efb99319fca9b1f115bf
FelicityRashetle20 <felichrist50@gmail.com>
2023-08-30 04:34:18 -0700

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for value in range(0, 100):
     if value != 99:
-        print(f"{value:02d}, ", end='')
+        print("{}".format('0' + str(value) if value < 10 else value), end='')
     else:
         print(value)
